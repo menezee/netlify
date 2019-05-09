10caaf775fd010380621c7bdfc0641cfa1d4645f
Erich Menezes <emenezes@thoughtworks.com>
2019-05-08 19:00:54 -0700

    :tada:

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,28 @@
+const questions = [
+  {
+    question: 'Quais são meus direitos?',
+    answer: 'Direito 1, Direito 2, e Direito 3',
+  },
+  {
+    question: 'Qual é o salário mínimo?',
+    answer: 'R$1.000,00 (mil)',
+  },
+  {
+    question: 'Quanto tempo de almoço eu tenho por dia?',
+    answer: '1h',
+  },
+  {
+    question: 'Quantos dias de férias tenho direito?',
+    answer: '30 dias por ano',
+  }
+]
+
 exports.handler = async event => {
-  const subject = event.queryStringParameters.name || 'World'
   return {
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
     statusCode: 200,
-    body: `Hello ${subject}!`,
+    body: JSON.stringify(questions),
   }
 }
